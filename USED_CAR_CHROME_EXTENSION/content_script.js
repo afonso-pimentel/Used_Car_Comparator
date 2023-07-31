1d15974296c50fd2cc2f3bca3173be5253ee096f
1151399 <1151399@isep.ipp.pt>
2023-08-01 00:39:37 +0100

    Converted table files to json. Refined the discount logic, added function to display the total price in the page

--- USED_CAR_CHROME_EXTENSION/content_script.js
+++ USED_CAR_CHROME_EXTENSION/content_script.js
@@ -1,73 +1,79 @@
+let engineTaxRates = {
+  normal: [],
+  cargoRvsOld: [],
+};
+
+let co2TaxRates = {
+  petrol: [],
+  diesel: [],
+};
+
+let engineDisplacementAgeDiscountTable = [];
+
+let co2EmissionsAgeDiscountTable = [];
+
+let carTypeDiscountTable = [];
+
+let engineDisplacementTax = 0;
+let co2EmissionTax = 0;
+let isvWithDiscount = 0;
+let carTotalPrice = 0;
+let carTotalPriceFormatted = "0";
+
+// Function to fetch JSON data from the corresponding files
+async function fetchJsonFile(filename) {
+  try {
+    const response = await fetch(
+      chrome.runtime.getURL("isvJsonTables/" + filename)
+    );
+    const jsonData = await response.json();
+
+    return jsonData;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+// Function to fill the variables with JSON data from the corresponding files
+async function loadJsonData() {
+  try {
+    const [
+      normal,
+      cargoRvsOld,
+      petrol,
+      diesel,
+      displacementDiscount,
+      emissionsDiscount,
+      carTypeDiscount,
+    ] = await Promise.all([
+      fetchJsonFile("Engine_displacement_normal.json"),
+      fetchJsonFile("Engine_displacement_carg_RV_Old.json"),
+      fetchJsonFile("Emissions_Petrol.json"),
+      fetchJsonFile("Emissions_Diesel.json"),
+      fetchJsonFile("Engine_displacement_discount.json"),
+      fetchJsonFile("Emissions_discount.json"),
+      fetchJsonFile("ISV_discount.json"),
+    ]);
+
+    engineTaxRates.normal = normal;
+    engineTaxRates.cargoRvsOld = cargoRvsOld;
+    co2TaxRates.petrol = petrol;
+    co2TaxRates.diesel = diesel;
+    engineDisplacementAgeDiscountTable = displacementDiscount;
+    co2EmissionsAgeDiscountTable = emissionsDiscount;
+    carTypeDiscountTable = carTypeDiscount;
+
+    console.log("JSON data loaded successfully!");
+  } catch (error) {
+    console.error("Error loading JSON data:", error);
+  }
+}
+
+// Function to extract car information from the website
 async function extractCarInformation() {
-  // Tax rates and discount tables
-  const engineTaxRates = {
-    normal: [
-      { maxDisplacement: 1000, rate: 1.04, parcel: 808.6 },
-      { maxDisplacement: 1250, rate: 1.12, parcel: 810.18 },
-      { maxDisplacement: Infinity, rate: 5.34, parcel: 5899.89 },
-    ],
-    cargoRvsOld: [
-      { maxDisplacement: 1250, rate: 5.05, parcel: 3173.03 },
-      { maxDisplacement: Infinity, rate: 11.98, parcel: 11560.45 },
-    ],
-  };
-
-  const co2TaxRates = {
-    petrol: [
-      { maxEmissions: 99, rate: 4.4, parcel: 406.67 },
-      { maxEmissions: 115, rate: 7.7, parcel: 715.23 },
-      { maxEmissions: 145, rate: 50.06, parcel: 5622.8 },
-      { maxEmissions: 175, rate: 58.32, parcel: 6800.16 },
-      { maxEmissions: 195, rate: 148.54, parcel: 22502.16 },
-      { maxEmissions: Infinity, rate: 195.86, parcel: 31800.11 },
-    ],
-    diesel: [
-      { maxEmissions: 79, rate: 5.5, parcel: 418.13 },
-      { maxEmissions: 95, rate: 22.33, parcel: 1760.55 },
-      { maxEmissions: 120, rate: 75.45, parcel: 6852.98 },
-      { maxEmissions: 140, rate: 167.36, parcel: 18023.73 },
-      { maxEmissions: 160, rate: 186.12, parcel: 20686.59 },
-      { maxEmissions: Infinity, rate: 255.64, parcel: 31855.14 },
-    ],
-  };
-
-  const engineDisplacementAgeDiscountTable = [
-    { maxAge: 1, discountPercentage: 10 },
-    { maxAge: 2, discountPercentage: 20 },
-    { maxAge: 3, discountPercentage: 28 },
-    { maxAge: 4, discountPercentage: 35 },
-    { maxAge: 5, discountPercentage: 43 },
-    { maxAge: 6, discountPercentage: 52 },
-    { maxAge: 7, discountPercentage: 60 },
-    { maxAge: 8, discountPercentage: 65 },
-    { maxAge: 9, discountPercentage: 70 },
-    { maxAge: 10, discountPercentage: 75 },
-    { maxAge: Infinity, discountPercentage: 80 },
-  ];
-
-  const co2EmissionsAgeDiscountTable = [
-    { maxAge: 2, discountPercentage: 10 },
-    { maxAge: 4, discountPercentage: 20 },
-    { maxAge: 6, discountPercentage: 28 },
-    { maxAge: 7, discountPercentage: 35 },
-    { maxAge: 9, discountPercentage: 43 },
-    { maxAge: 10, discountPercentage: 52 },
-    { maxAge: 12, discountPercentage: 60 },
-    { maxAge: 13, discountPercentage: 65 },
-    { maxAge: 14, discountPercentage: 70 },
-    { maxAge: 15, discountPercentage: 75 },
-    { maxAge: Infinity, discountPercentage: 80 },
-  ];
-
-  const carTypeDiscountTable = {
-    hybrids: { percentageToPay: 60, discountPercentage: 40 },
-    plugInHybrids: { percentageToPay: 25, discountPercentage: 75 },
-    naturalGas: { percentageToPay: 40, discountPercentage: 60 },
-    seats7: { percentageToPay: 40, discountPercentage: 60 },
-  };
-
   // Function to extract data for mobile.de
-  function extractMobileDeData() {
+  async function extractMobileDeData() {
     const carPriceElement = document.querySelector(
       '[data-testid="prime-price"]'
     );
@@ -76,23 +82,27 @@ async function extractCarInformation() {
     const carEmissionsElement = document.querySelector("#envkv\\.emission-v");
     const carFuelElement = document.querySelector("#fuel-v");
     const carSeatsElement = document.querySelector("#numSeats-v");
+    const missingElements = [];
+    let errorMessage = "";
 
-    if (
-      carPriceElement &&
-      carYearElement &&
-      carEngineElement &&
-      carEmissionsElement &&
-      carFuelElement &&
-      carSeatsElement
-    ) {
+    if (!carPriceElement) missingElements.push("Car price");
+    if (!carYearElement) missingElements.push("Car year");
+    if (!carEngineElement) missingElements.push("Car engine displacement");
+    if (!carEmissionsElement) missingElements.push("Car emissions");
+    if (!carFuelElement) missingElements.push("Car fuel type");
+    if (!carSeatsElement) missingElements.push("Car seats");
+
+    if (missingElements.length != 0) {
+      const missingElementsString = missingElements.join(", ");
+      errorMessage = `Missing elements: ${missingElementsString}`;
+      return errorMessage;
+    } else {
       const carPrice = carPriceElement.textContent.trim();
       const carYear = carYearElement.textContent.trim();
       const carEngineDisplacement = carEngineElement.textContent.trim();
       const carEmissions = carEmissionsElement.textContent.trim();
       const carFuel = carFuelElement.textContent.trim();
       const carSeats = parseInt(carSeatsElement.textContent.trim());
-
-      // Additional processing if needed, e.g., converting strings to numbers
 
       console.log("Car Price:", carPrice);
       console.log("Car Year:", carYear);
@@ -109,8 +119,8 @@ async function extractCarInformation() {
         : 0;
 
       // Calculate Engine Displacement Tax based on the tables provided
-      let engineDisplacementTax = 0;
       let taxRates = engineTaxRates.normal;
+
       // if (carFuelElement.textContent.trim().toLowerCase() === "diesel") {
       //   taxRates = engineTaxRates.cargoRvsOld;
       // }
@@ -120,6 +130,11 @@ async function extractCarInformation() {
           break;
         }
       }
+
+      console.log(
+        "Engine Displacement Tax before Discount:",
+        engineDisplacementTax
+      );
       // Apply age discount for engine displacement tax
       engineDisplacementTax *=
         1 - getAgeDiscount(carYear, engineDisplacementAgeDiscountTable);
@@ -133,8 +148,7 @@ async function extractCarInformation() {
       const carType = carFuelElement.textContent.trim().toLowerCase();
 
       // Calculate CO2 Emission Tax based on the tables provided
-      let co2EmissionTax = 0;
-      if (carType === "diesel") {
+      if (carType.includes("diesel")) {
         taxRates = co2TaxRates.diesel;
       } else {
         taxRates = co2TaxRates.petrol;
@@ -145,6 +159,8 @@ async function extractCarInformation() {
           break;
         }
       }
+
+      console.log("CO2 Emission Tax before Discount:", co2EmissionTax);
       // Apply age discount for CO2 emission tax
       co2EmissionTax *=
         1 - getAgeDiscount(carYear, co2EmissionsAgeDiscountTable);
@@ -153,14 +169,22 @@ async function extractCarInformation() {
       const isv = engineDisplacementTax + co2EmissionTax;
 
       // Apply additional discounts based on car type (hybrids, plug-in hybrids, etc.)
-      const isvWithDiscount = applyCarTypeDiscount(isv, carType, carSeats);
+      isvWithDiscount = applyCarTypeDiscount(isv, carType, carSeats);
+
+      carTotalPrice = parseFloat(carPrice.replace(".", "")) + isvWithDiscount;
+
+      carTotalPriceFormatted = (carTotalPrice / 1000).toLocaleString("de-DE", {
+        style: "currency",
+        currency: "EUR",
+        minimumFractionDigits: 3,
+        maximumFractionDigits: 3,
+      });
 
       console.log("Engine Displacement Tax:", engineDisplacementTax);
       console.log("CO2 Emission Tax:", co2EmissionTax);
       console.log("ISV with Discounts:", isvWithDiscount);
-    } else {
-      console.log("Failed to extract data from mobile.de");
-      // Implement a fallback option or show a message to the user.
+      console.log("Car Total Price:", carTotalPriceFormatted);
+      return carTotalPriceFormatted;
     }
   }
 
@@ -184,6 +208,8 @@ async function extractCarInformation() {
       carAge++; // Adjust for months (if the current month is later than the registration month)
     }
 
+    console.log("Car Age:", carAge);
+
     // Find the corresponding discount percentage from the discountTable
     let discountPercentage = 0;
     for (const entry of discountTable) {
@@ -198,17 +224,19 @@ async function extractCarInformation() {
 
   // Function to apply additional discounts based on car type (hybrids, plug-in hybrids, etc.)
   function applyCarTypeDiscount(isv, carFuel, carSeats) {
-    const carTypeKey = getCarTypeKey(carFuel, carSeats);
+    const carType = carTypeDiscountTable.find(item => {
+      return item.type === getCarTypeKey(carFuel, carSeats);
+    });
 
-    if (carTypeKey in carTypeDiscountTable) {
-      const { percentageToPay, discountPercentage } =
-        carTypeDiscountTable[carTypeKey];
+    if (carType) {
+      const { percentageToPay, discountPercentage } = carType;
       return isv * (1 - discountPercentage / 100);
     }
 
     return isv;
   }
 
+  // Function to get the car type key from the car fuel and number of seats
   function getCarTypeKey(carFuel, carSeats) {
     const carTypeMap = {
       diesel: "diesel",
@@ -216,25 +244,30 @@ async function extractCarInformation() {
       elektro: "elektro",
       "ethanol (ffv, e85 etc.)": "other",
       hybrids: "hybrids",
-      "autogas (lpg)": "other",
-      "erdgas (cng)": "naturalGas",
+      plugInHybrids: "plugInHybrids",
+      naturalGas: "naturalGas",
+      seats7: "seats7",
       wasserstoff: "other",
       andere: "other",
     };
 
     const carTypeKey = carFuel.toLowerCase().includes("plug-in-hybrid")
       ? "plugInHybrids"
+      : carSeats >= 7
+      ? "seats7"
+      : carFuel.toLowerCase().includes("Autogas")
+      ? "naturalGas"
       : carFuel.toLowerCase().includes("hybrid")
       ? "hybrids"
       : carFuel.toLowerCase().trim();
-    return carTypeMap[carTypeKey] + (carSeats >= 7 ? "-seats7" : "");
+    return carTypeMap[carTypeKey];
   }
 
   // Function to determine the website and call the appropriate extraction function
   function determineWebsiteAndExtractData() {
     const currentUrl = window.location.href;
     if (currentUrl.includes("mobile.de")) {
-      extractMobileDeData();
+      return extractMobileDeData();
       // Add more cases for other websites if needed
     } else {
       console.log("Website not supported.");
@@ -242,8 +275,81 @@ async function extractCarInformation() {
     }
   }
 
-  determineWebsiteAndExtractData();
+  return determineWebsiteAndExtractData();
 }
 
-// Call the function to extract car information when the content script is executed.
-extractCarInformation();
+// Function to convert the string "infinity" to Infinity
+function convertInfinity(data) {
+  for (const item of data) {
+    for (const key in item) {
+      if (item[key] === "infinity") {
+        item[key] = Infinity;
+      }
+    }
+  }
+}
+
+// Function to display the total price on the website
+function displayTotalPrice(totalPrice) {
+  // Identify the element where you want to display the total price
+  const carPriceElement = document.querySelector(
+    "#main-cta-box-price-row > div.main-price-and-rating-row"
+  );
+  if (carPriceElement) {
+    // Create a new element to display the total price
+    const totalPriceElement = document.createElement("span");
+
+    // Create a new element for the "c/ISV: " part with a smaller font size
+    const prefixElement = document.createElement("span");
+    prefixElement.style.fontSize = "1rem"; // Adjust the font size as needed
+    prefixElement.textContent = "c/ISV: ";
+
+    // Create a new element for the total price value
+    const valueElement = document.createElement("span");
+    valueElement.style.fontSize = "1.5rem";
+    valueElement.style.fontWeight = "bold";
+    valueElement.style.color = "#008000";
+    valueElement.textContent = totalPrice; // Format the total price as a currency string
+
+    // Append the prefix and value elements to the total price element
+    totalPriceElement.appendChild(prefixElement);
+    totalPriceElement.appendChild(valueElement);
+
+    // Insert the total price element in front or below the car price element
+    // You can choose either "beforebegin" or "afterend" depending on the desired location
+    carPriceElement.insertAdjacentElement("afterend", totalPriceElement);
+
+    // Create a blank div for spacing
+    const blankDivElement = document.createElement("div");
+    blankDivElement.style.height = "20px"; // Adjust the height as needed
+
+    // Insert the blank div after the total price element
+    totalPriceElement.insertAdjacentElement("afterend", blankDivElement);
+  }
+}
+
+// Function to execute the code
+async function main() {
+  await loadJsonData(); // Ensure loadJsonData is called before any code that uses the variables
+
+  convertInfinity(engineTaxRates.normal);
+  convertInfinity(engineTaxRates.cargoRvsOld);
+  convertInfinity(co2TaxRates.petrol);
+  convertInfinity(co2TaxRates.diesel);
+  convertInfinity(engineDisplacementAgeDiscountTable);
+  convertInfinity(co2EmissionsAgeDiscountTable);
+  // Now the variables should have Infinity instead of the string "infinity"
+
+  // Call the function to extract car information when the content script is executed.
+  carTotalPriceFormatted = await extractCarInformation();
+  if (
+    carTotalPriceFormatted &&
+    carTotalPriceFormatted.includes("Missing elements")
+  ) {
+    console.error("Error extracting car information:", carTotalPriceFormatted);
+  }
+
+  displayTotalPrice(carTotalPriceFormatted);
+}
+
+main();
