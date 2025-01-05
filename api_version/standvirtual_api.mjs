cdfe035ccce73a02b671eb5f3e3b8cd087f31377
1151399 <1151399@isep.ipp.pt>
2025-01-05 22:23:59 +0000

    Changed some values to floats and externalized the filters into a json file.

--- api_version/standvirtual_api.mjs
+++ api_version/standvirtual_api.mjs
@@ -32,8 +32,12 @@ async function fetchAds(filters, outputFile) {
         name: ad.title || "N/A",
         price: {
           value:
-            parseFloat(ad.gross_price?.replace(" EUR", "").replace(",", "")) ||
-            "N/A",
+            parseFloat(
+              ad.gross_price
+                ?.replace(" EUR", "")
+                .replace(",", "")
+                .replace(" ", "")
+            ) || "N/A",
           currency: "EUR",
         },
         url: ad.url || "N/A",
@@ -41,17 +45,24 @@ async function fetchAds(filters, outputFile) {
         car_details: {
           model: ad.params.find(param => param[0] === "Modelo")?.[1] || "N/A",
           productionDate:
-            ad.params.find(param => param[0] === "Ano")?.[1]?.trim() || "N/A",
+            parseFloat(
+              ad.params.find(param => param[0] === "Ano")?.[1]?.trim()
+            ) || "N/A",
           mileage:
-            ad.params
-              .find(param => param[0] === "Quilómetros")?.[1]
-              ?.replace(" km", "")
-              .replace(",", "") || "N/A",
+            parseFloat(
+              ad.params
+                .find(param => param[0] === "Quilómetros")?.[1]
+                ?.replace(" km", "")
+                .replace(",", "")
+                .replace(" ", "")
+            ) || "N/A",
           engineDetails: {
             enginePower:
-              ad.params
-                .find(param => param[0] === "Potência")?.[1]
-                ?.replace(" cv", "") || "N/A",
+              parseFloat(
+                ad.params
+                  .find(param => param[0] === "Potência")?.[1]
+                  ?.replace(" cv", "")
+              ) || "N/A",
             fuelType:
               ad.params.find(param => param[0] === "Combustível")?.[1] || "N/A",
           },
@@ -74,19 +85,8 @@ async function fetchAds(filters, outputFile) {
   }
 }
 
-// Example usage with filters
-const filters = {
-  "search[filter_enum_make]": "bmw",
-  "search[filter_enum_model]": "120",
-  "search[filter_enum_engine_code]": "serie-1",
-  "search[filter_enum_fuel_type]": "diesel",
-  "search[filter_float_engine_power:from]": "100",
-  "search[filter_float_engine_power:to]": "200",
-  "search[filter_float_first_registration_year:to]": "2024",
-  "search[filter_float_mileage:from]": "25000",
-  "search[filter_float_mileage:to]": "200000",
-  "search[filter_float_price:from]": "10000",
-  "search[filter_float_price:to]": "40000",
-};
+// Get the filters from the JSON file
+const filters = JSON.parse(fs.readFileSync("filters.json", "utf-8"));
+console.log(filters);
 
 fetchAds(filters, "ads.json");
